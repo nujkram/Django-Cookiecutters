18dcecb97da58e4860672f17eb1e2a31d7d4110b
Jorge Cosgayon <jcosgayon@serviotech.com>
2020-01-18 17:02:13 +0800

    Safe admin URL

--- django/{{cookiecutter.project_slug}}/accounts/models/account/constants.py
+++ django/{{cookiecutter.project_slug}}/accounts/models/account/constants.py
@@ -8,8 +8,8 @@ USERNAME_REGEX = "^[a-zA-Z0-9.-]*$"
 try:
     with open(os.path.join(BASE_DIR, 'ADMIN_URL')) as f:
         ADMIN_URL = f.read().strip()
-except FileNotFoundError:
-    generated_key = ''.join([random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(50)])
+except FileNotFoundError:   
+    generated_key = ''.join(random.choice(string.ascii_lowercase) for i in range(50))
     secret = open(os.path.join(BASE_DIR, 'ADMIN_URL'), 'w')
     secret.write(generated_key)
     secret.close()
